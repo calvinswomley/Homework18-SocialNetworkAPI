55c52a131036ae1aebe11ce635b62c1ca2254d80
Calvin Swomley <calvinswomley@gmail.com>
2022-01-23 12:22:38 -0700

    Controllers created

---
+++ Develop/Controllers/thoughtController.js
@@ -0,0 +1,91 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    //get all thoughts
+    getThoughts(req, res){
+        Thoughts.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+    //get single thoughts
+    getSingleThought(req, res){
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then(async (thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json({
+                    thought,
+            })
+            )
+            .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+            });
+    },
+    //create thought
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
+    },
+    //update thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+          )
+            .then((course) =>
+              !course
+                ? res.status(404).json({ message: 'No course with this id!' })
+                : res.json(course)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    //delete thought
+    deleteThought(req, res){
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : Thought.deleteMany({ _id: { $in: thought.reactions } })
+        )
+        .then(() => res.json({ message: 'Course and students deleted!' }))
+        .catch((err) => res.status(500).json(err));
+    },
+    //creation reaction
+    createReaction(req, res){
+        console.log('You are adding a reaction.');
+        console.log(req.body);
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+        .then((thought) =>
+            !thought
+                ? res
+                    .status(404)
+                    .json({ message: 'No thought found with that ID.' })
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    //delete reaction
+    deleteReaction(req, res){
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reaction: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+          )
+            .then((thought) =>
+              !thought
+                ? res
+                    .status(404)
+                    .json({ message: 'No thought found with that ID.' })
+                : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+};
--- Develop/Controllers/userController.js
+++ Develop/Controllers/userController.js
@@ -43,15 +43,41 @@ module.exports = {
             .then((course) =>
                 !user
                     ? res.status(404).json({ message: 'No user with that ID' })
-                    : User.deleteMany({ _id: { $in: user.thoughts } })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
             )
             .then(() => res.json({ message: 'User and thoughts deleted!' }))
             .catch((err) => res.status(500).json(err));
     },
     addNewFriend(req, res){
-        
+        console.log('You are adding a friend.');
+        console.log(req.body);
+        Student.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res
+                        .status(404)
+                        .json({ message: 'No user found with that ID.' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     removeFriend(req, res){
-
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend: { userId: req.params.userId } } },
+            { runValidators: true, new: true }
+          )
+            .then((user) =>
+              !user
+                ? res
+                    .status(404)
+                    .json({ message: 'No user found with that ID.' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     },
 };
